a69de458f5a4115e7e4074f3711a2044d14dda0b
xianghuy <xianghuy@opera.com>
2014-06-20 17:58:48 +0800

    add auto-java-complete

--- configure/autocomplete-configure.el
+++ configure/autocomplete-configure.el
@@ -9,4 +9,11 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
+
+;;for auto-java-complete
+(require 'ajc-java-complete-config)
+(add-hook 'java-mode-hook 'ajc-java-complete-mode)
+(add-hook 'find-file-hook 'ajc-4-jsp-find-file-hook)
+(custom-set-variables '(ajc-tag-file "~/.emacs.d/configure/.java_base.tag"))
+
 (provide 'autocomplete-configure)
--- module/module.el
+++ module/module.el
@@ -1,3 +1,3 @@
 (add-to-list 'load-path' "~/.emacs.d/module/redspace")
-
+(add-to-list 'load-path' "~/.emacs.d/module/auto-java-complete")
 (provide 'module)
