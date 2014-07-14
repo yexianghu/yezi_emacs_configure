4acd337760da73e9d5698080d9f55a472a0332bd
xianghuy <xianghuy@opera.com>
2014-07-14 21:10:15 +0800

    add ggtags.el

--- module/module.el
+++ module/module.el
@@ -2,4 +2,5 @@
 (add-to-list 'load-path' "~/.emacs.d/module/auto-java-complete")
 (add-to-list 'load-path' "~/.emacs.d/module/jdee/jdee-2.4.1/lisp")
 (add-to-list 'load-path' "~/.emacs.d/module/ido-custom")
+(add-to-list 'load-path' "~/.emacs.d/module/ggtags")
 (provide 'module)
