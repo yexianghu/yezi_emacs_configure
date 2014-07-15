e19cd196b12de26eacd603f271aa18f834877f01
xianghuy <xianghuy@opera.com>
2014-07-15 09:15:45 +0800

    add code

--- configure/ggtags-configure.el
+++ configure/ggtags-configure.el
@@ -2,5 +2,9 @@
 
 (setq ggtags-executable-directory "~/bin/")
 
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
 
 (provide 'ggtags-configure)
