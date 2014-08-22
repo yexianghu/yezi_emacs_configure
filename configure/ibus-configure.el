c3b90e92ec2233c3dc399e453d0d4da86d8aa1ce
xianghuy <xianghuy@opera.com>
2014-08-22 11:09:06 +0800

    add for ibus

--- configure/ibus-configure.el
+++ configure/ibus-configure.el
@@ -2,5 +2,5 @@
 
 (add-hook 'after-init-hook 'ibus-mode-on)
 (setq ibus-agent-file-name "~/.emacs.d/module/ibus/ibus-el-0.3.2/ibus-el-agent")
-
+(global-set-key (kbd "M-#") 'ibus-toggle)
 (provide 'ibus-configure)
